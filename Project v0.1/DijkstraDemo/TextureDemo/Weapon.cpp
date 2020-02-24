160c4ff411c708352fb72b65718b4e4f44789dd5
Andrew <andrewl007@hotmail.com>
2020-02-24 12:39:28 -0500

    make bullet class, weapon class

--- Project v0.1/DijkstraDemo/TextureDemo/Weapon.cpp
+++ Project v0.1/DijkstraDemo/TextureDemo/Weapon.cpp
@@ -3,6 +3,7 @@
 Weapon::Weapon(glm::vec3& entityPos, GLuint entityTexture, GLint entityNumElements, std::string newType)
 	: GameObject(entityPos, entityTexture, entityNumElements,newType)
 {
+
 }
 
 //copy weapon object from stroe to player
@@ -11,3 +12,17 @@ Weapon::Weapon(Weapon& w)
 {
 
 }
+
+void Weapon::update(double deltaTime) {
+
+	if (curfirerateTimer > 0) { 
+		curfirerateTimer -= 1; 
+	}
+	else {
+		if (glfwGetKey(Window::getWindow(), GLFW_KEY_SPACE) == GLFW_PRESS) {
+			curfirerateTimer = firerate;
+		}
+	}
+
+}
+
--- Project v0.1/DijkstraDemo/TextureDemo/Weapon.h
+++ Project v0.1/DijkstraDemo/TextureDemo/Weapon.h
@@ -1,10 +1,24 @@
 #pragma once
 #include "GameObject.h"
+#include "Window.h"
 class Weapon :
 	public GameObject
 {
 public:
-		Weapon(glm::vec3& entityPos, GLuint entityTexture, GLint entityNumElements, std::string newType);
+		Weapon(glm::vec3& entityPos, GLuint entityTexture, GLint entityNumElements, std::string newType,float damage,float firerate,float bulletSpeed,int ammo, int prize,float fireRange);
 		Weapon(Weapon& w);
+
+		void update(double deltaTime);
+		
+
+private:
+	float damage;
+	float firerate;
+	float bulletSpeed;
+	int ammo;
+	int prize;
+	float fireRange;
+	float curfirerateTimer;
+
 };
 
