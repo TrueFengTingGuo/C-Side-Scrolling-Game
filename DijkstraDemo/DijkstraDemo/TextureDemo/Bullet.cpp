ccafbb65e41b8c5de6d2a845c8dff94de5f27e13
FengTing Guo <15019668+TrueFengTingGuo@users.noreply.github.com>
2020-03-04 15:45:08 -0500

    delete

--- DijkstraDemo/DijkstraDemo/TextureDemo/Bullet.cpp
+++
@@ -1,8 +0,0 @@
-#include "Bullet.h"
-
-Bullet::Bullet(GameObjectHandler* h, glm::vec3& entityPos, GLuint entityTexture, GLint entityNumElements, std::string newType, float d, std::string bt)
-	: GameObject(h, entityPos, entityTexture, entityNumElements, newType)
-{
-	damage = d;
-	bulletType = bt;
-}
